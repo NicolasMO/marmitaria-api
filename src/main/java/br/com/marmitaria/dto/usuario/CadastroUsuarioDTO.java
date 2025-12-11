d1580f258141063dd33e5128e5fe66c1790652ba
Nicolas Oliveira <nicolas_oliveira97@hotmail.com>
2025-12-11 14:49:01 -0300

    fix/ajustes na tabela usuarios para condizer com entidade atual e melhoria de validação no DTO de cadastro

--- src/main/java/br/com/marmitaria/dto/usuario/CadastroUsuarioDTO.java
+++ src/main/java/br/com/marmitaria/dto/usuario/CadastroUsuarioDTO.java
@@ -7,6 +7,10 @@ import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record CadastroUsuarioDTO(
+        @Pattern(
+                regexp = "^[A-Za-zÀ-ÿ ]+$",
+                message = "O nome deve conter apenas letras e espaços."
+        )
         @NotBlank(message = "Nome é obrigatório")
         String nome,
 
@@ -23,7 +27,9 @@ public record CadastroUsuarioDTO(
         String celular,
 
         @NotBlank(message = "Senha é obrigatória")
-        @Size(min = 6, message = "Senha curta, mínimo de 6 caracteres")
-        @Size(max = 20, message = "Senha com máximo de 20 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%])[A-Za-z\\d!@#$%]{6,20}$",
+                message = "A senha deve ter 6–20 caracteres, contendo ao menos 1 letra maiúscula, 1 minúscula, 1 número e 1 símbolo (!@#$%)."
+        )
         String senha
 ) {}
